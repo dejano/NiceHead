daf41eed05c91971d08c2d89475bfe20e0052c5c
nikolavukadinov42 <nikola.vukadinov42@gmail.com>
2015-05-18 20:35:35 +0200

    [#93744030] update xsd sema

--- CentralnaBanka/src/rs/ac/uns/ftn/xws/bo/mt102/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.ftn.uns.ac.rs/xws/bo/mt102", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package rs.ac.uns.ftn.xws.bo.mt102;
