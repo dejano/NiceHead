eb149ae2099c21c07a829890f66b79f7eb308378
nikolavukadinov42 <nikola.vukadinov42@gmail.com>
2015-05-19 12:12:14 +0200

    sinhronizacija cb i b

--- Banka/src/main/java/rs/ac/uns/ftn/xws/generated/RtgsRequestMessage.java
+++ Banka/src/main/java/rs/ac/uns/ftn/xws/generated/RtgsRequestMessage.java
@@ -8,16 +8,15 @@ import javax.xml.bind.annotation.XmlType;
 
 
 /**
- * <p>Java class for RtgsConfirmMessage complex type.
+ * <p>Java class for RtgsRequestMessage complex type.
  * 
  * <p>The following schema fragment specifies the expected content contained within this class.
  * 
  * <pre>
- * &lt;complexType name="RtgsConfirmMessage">
+ * &lt;complexType name="RtgsRequestMessage">
  *   &lt;complexContent>
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
- *         &lt;element name="mt910" type="{http://www.ftn.uns.ac.rs/xws/xsd/mt910}Mt910"/>
  *         &lt;element name="mt103" type="{http://www.ftn.uns.ac.rs/xws/xsd/mt103}Mt103"/>
  *       &lt;/sequence>
  *     &lt;/restriction>
@@ -28,40 +27,13 @@ import javax.xml.bind.annotation.XmlType;
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "RtgsConfirmMessage", propOrder = {
-    "mt910",
+@XmlType(name = "RtgsRequestMessage", propOrder = {
     "mt103"
 })
-public class RtgsConfirmMessage {
+public class RtgsRequestMessage {
 
-    @XmlElement(required = true)
-    protected Mt910 mt910;
     @XmlElement(required = true)
     protected Mt103 mt103;
-
-    /**
-     * Gets the value of the mt910 property.
-     * 
-     * @return
-     *     possible object is
-     *     {@link Mt910 }
-     *     
-     */
-    public Mt910 getMt910() {
-        return mt910;
-    }
-
-    /**
-     * Sets the value of the mt910 property.
-     * 
-     * @param value
-     *     allowed object is
-     *     {@link Mt910 }
-     *     
-     */
-    public void setMt910(Mt910 value) {
-        this.mt910 = value;
-    }
 
     /**
      * Gets the value of the mt103 property.
